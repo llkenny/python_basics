38bc256f802d9787b57213145edbac25233a9f93
Maxim Doroshenko <m.s.doroshenko@gmail.com>
2020-11-12 13:33:06 +0300

    Add Exercise 3 2

---
+++ Lesson 3/exercise_2.py
@@ -0,0 +1,13 @@
+# Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя: имя, фамилия, год рождения, город проживания, email, телефон.
+# Функция должна принимать параметры как именованные аргументы.
+# Реализовать вывод данных о пользователе одной строкой.
+
+def user(**kwargs):
+    print(f'Name: {kwargs["name"]}, Surname: {kwargs["surname"]}, Date of Birth: {kwargs["dob"]}, City: {kwargs["city"]}, Email: {kwargs["email"]}, Phone: {kwargs["phone"]}')
+
+kwargs = {"name": "", "surname": "", "dob": "", "city": "", "email": "", "phone": ""}
+for key in kwargs.keys():
+    value = input(f'Enter {key}: ')
+    kwargs[key] = value
+
+user(**kwargs)
